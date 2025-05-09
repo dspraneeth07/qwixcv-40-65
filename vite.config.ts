e8f10e4726491382b81e8cfbff12c9c6b23aa507
SAI PRANEETH REDDY DHADI <dspraneeth07@gmail.com>
2025-05-09 19:15:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -8,6 +9,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "eaba0716-c18e-439c-917e-9316b74385b7.lovableproject.com",
+      "localhost",
+      "127.0.0.1"
+    ],
   },
   plugins: [
     react(),
